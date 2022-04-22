dfb7c339956b0eb2e8712d5eea63e25f86a0e2d9
AleksandrKolenov <kolenov.aa@yandex.ru>
2022-04-22 22:13:40 +0300

    123

---
+++ src/js/02-timer.js
@@ -0,0 +1,100 @@
+// import 'flatpickr/dist/flatpickr.min.css';
+// import flatpickr from 'flatpickr';
+
+// const refs = {
+//   timer: document.querySelector('#datetime-picker'),
+//   btn: document.querySelector('button'),
+//   days: document.querySelector('[data-days]'),
+//   hours: document.querySelector('[data-hours]'),
+//   minutes: document.querySelector('[data-minutes]'),
+//   seconds: document.querySelector('[data-seconds]'),
+// };
+// let thisID = null;
+// refs.btn.setAttribute('disabled', true);
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//     // console.log(Date.parse(selectedDates[0]));
+//     // console.log(Date.now());
+//     if (Date.parse(selectedDates[0]) > Date.now()) {
+//       refs.btn.removeAttribute('disabled');
+//       let delayTime = Date.parse(selectedDates[0]) - Date.now();
+//     const delayTimeObj = convertMs(delayTime);
+//     refs.days.textContent = `${pad(delayTimeObj.days)}`;
+//     refs.hours.textContent = `${pad(delayTimeObj.hours)}`;
+//     refs.minutes.textContent = `${pad(delayTimeObj.minutes)}`;
+//     refs.seconds.textContent = `${pad(delayTimeObj.seconds)}`;
+// clearInterval(thisID);
+//     // return
+//     } else {
+//       alert('Ведите более позднюю дату');
+//     }
+//   },
+// };
+
+// const dateEnjoer = flatpickr(refs.timer, options);
+
+// refs.btn.addEventListener('click', start);
+
+
+// function start() {
+    
+//   const startTime = Date.parse(dateEnjoer.selectedDates[0]);
+//   refs.btn.setAttribute('disabled', true);
+//   thisID = setInterval(() => {
+//     const currentTime = Date.now();
+//     let delayTime = startTime - currentTime;
+//     const delayTimeObj = convertMs(delayTime);
+   
+//     refs.days.textContent = `${pad(delayTimeObj.days)}`;
+//     refs.hours.textContent = `${pad(delayTimeObj.hours)}`;
+//     refs.minutes.textContent = `${pad(delayTimeObj.minutes)}`;
+//     refs.seconds.textContent = `${pad(delayTimeObj.seconds)}`;
+//     if(Math.round(delayTime/1000)===0) {
+//         console.log('Отсчет окончен')
+//         clearInterval(thisID);
+// return
+//     }
+    
+//   }, 1000);
+  
+// }
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+
+//   function pad(value) {
+//       return String(value).padStart(2, '0')
+//   }
+
+function findNeedle(haystack) {
+  const haystack1 = haystack.indexOf('needle');
+  const current = haystack.includes('needle');
+  if (current) {
+    return `found the needle at position ${haystack1}`
+  }
+  
+}
+console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]));
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,27 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// import Notiflix from 'notiflix';
+
+// const prom = new Promise((resole, reject) => {
+//   const cd = Math.random() > 0.5;
+
+//   if (cd) {
+//     resole('jnjiniuu (исполнен)');
+//   }
+
+//   reject('iuhhhu(отклонён)');
+// })
+
+// console.log(prom);
+
+
+function even_or_odd(number) {
+  for (let i = 0; i < number.length; i++) {
+ if (number) {
+   even % 2 === 0;
+   return even;
+ }if (number) {
+   odd % 2 !== 0;
+   return odd;
+ }
   }
-}
+};
+
