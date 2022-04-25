a55799032cbf81d25692e097aa8e716f1cca8960
AleksandrKolenov <kolenov.aa@yandex.ru>
2022-04-25 21:13:18 +0300

    7987

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,27 +1,52 @@
-// import Notiflix from 'notiflix';
-
-// const prom = new Promise((resole, reject) => {
-//   const cd = Math.random() > 0.5;
-
-//   if (cd) {
-//     resole('jnjiniuu (исполнен)');
-//   }
-
-//   reject('iuhhhu(отклонён)');
-// })
-
-// console.log(prom);
-
-
-function even_or_odd(number) {
-  for (let i = 0; i < number.length; i++) {
- if (number) {
-   even % 2 === 0;
-   return even;
- }if (number) {
-   odd % 2 !== 0;
-   return odd;
- }
-  }
+import Notiflix from 'notiflix';
+const refs = {
+  form: document.querySelector('form'),
+  delay: document.querySelector('input[name = delay]'),
+  step: document.querySelector('input[name =step]'),
+  amount: document.querySelector('input[name =amount]'),
 };
+let setID = null;
+let DATA = [];
+refs.form.addEventListener('input', onPromise);
+refs.form.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefault();  
+  delayBilder(refs.amount, refs.delay, refs.step);
+  DATA.forEach((currentValue, index) => {   
+    createPromise(index, currentValue).then(onSucsess).catch(onError);
+  });
+  DATA = [];
+clearTimeout(setID);
+}
+function onSucsess(result) {  
+  Notiflix.Notify.success(`${result}`);
+}
+function onError(error) {  
+  Notiflix.Notify.warning(`${error}`);
+}
+function onPromise(e) {
+  refs[e.target.name] = e.target.value;  
+}
+function delayBilder(amount, delay, step) {
+  DATA = []
+  for (let i = 0; i < amount; i += 1) {
+    delay = Number(delay);
+    step = Number(step);
+    const total = delay + step*i     
+    DATA.push(total);
+    console.log(DATA);
+  }
+}
+function createPromise(position, delay) {
+  return new Promise((reslove, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    const setID = setTimeout(() => {
+      if (shouldResolve) {
+        reslove(`✅ Fulfilled promise ${position+1} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position+1} in ${delay}ms`);
+    }, delay);
+  });
+}
+
 
